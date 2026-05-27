53dd2ae2fd6413b82372595253e94b90e3f2fb7d
Swintie <catexe5200@gmail.com>
2026-05-28 03:25:20 +0500

    Adding weird ascii password generation

--- passgen.py
+++ passgen.py
@@ -1,3 +1,7 @@
+from random import choice
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+
+
 def confirmation() -> bool:
     confirm = input("Do you want to continue? [Y/n]: ").lower()
     if confirm in ("yes", "y", ""):
@@ -5,6 +9,42 @@ def confirmation() -> bool:
         return True
     else:
         return False
+
+
+dictionaries = {
+    "1": ascii_lowercase,
+    "2": ascii_uppercase,
+    "3": digits,
+    "4": punctuation,
+}
+
+
+def select_dict():
+    selection = input(
+        f"Select a dictionary(es):\n"
+        f"[1] {ascii_lowercase}\n"
+        f"[2] {ascii_uppercase}\n"
+        f"[3] {digits}\n"
+        f"[4] {punctuation}\n"
+        f"Your choice: "
+    )
+    return selection
+
+
+def gen_ascii_pass() -> None:  # This generator is't ideal but it's working at least
+    try:
+        length = int(input("Password length: "))
+    except ValueError:
+        print("You must enter an integer value")
+        return
+    work_dir = "".join(dictionaries[i] for i in select_dict() if i in dictionaries)
+    if not work_dir:
+        print("Dictionary is empty")
+        return
+    password = ""
+    for i in range(length):
+        password += choice(work_dir)
+    print(password)
 
 
 def gen_zeros(file_path) -> None:
@@ -40,14 +80,17 @@ def gen_numbers(file_path) -> None:
 
 if __name__ == "__main__":
     try:
-        location = input("Location of the output file: ")
         option = input(
-            "Select an option\n[1] Fill file with zeros\n[2] Generate number passwords\n[1/2]: "
+            "Select an option\n[1] Fill file with zeros\n[2] Generate number passwords\n[3] Generate custom password\n: "
         )
         if option == "1":
+            location = input("Location of the output file: ")
             gen_zeros(location)
         elif option == "2":
+            location = input("Location of the output file: ")
             gen_numbers(location)
+        elif option == "3":
+            gen_ascii_pass()
         else:
             print("Incorrect option!!!")
     except KeyboardInterrupt:
